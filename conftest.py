2c7e284c47adc3f6a13493edf34ec07fbbfcf00d
Akarsh <Akarsh@pyramidconsultinginc.com>
2026-05-30 18:01:07 +0530

    Sprint 7 - Added hooks and automatic failure capture

--- conftest.py
+++ conftest.py
@@ -7,3 +7,35 @@ from core.llm_client import LLMClient
 def llm_client():
 
     return LLMClient()
+
+
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(
+        item,
+        call
+):
+
+    outcome = yield
+
+    report = outcome.get_result()
+
+    if report.when == "call":
+
+        if report.failed:
+
+            print(
+                f"\nFAILED TEST: {item.name}"
+            )
+
+            if "data" in item.funcargs:
+
+                test_data = item.funcargs[
+                    "data"
+                ]
+
+                print(
+                    f"Question: "
+                    f"{test_data['question']}"
+                )
